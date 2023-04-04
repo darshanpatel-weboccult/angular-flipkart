f93f0b9c6ee93b3883ae59e69fc6304c0eabfec0
Darshan Patel <darshan.patel@weboccult.com>
2023-04-04 15:49:54 +0530

    added Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarModule } from './Components/Partials/navbar/navbar.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterModule } from './Components/Partials/footer/footer.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     AppRoutingModule,
     FontAwesomeModule,
     NavbarModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
