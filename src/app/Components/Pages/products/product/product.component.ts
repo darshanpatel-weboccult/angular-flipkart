bcc202041130003df702b3016978db91bf4596a1
Darshan Patel <darshan.patel@weboccult.com>
2023-04-12 17:09:02 +0530

    Product Page (Single): added image gallery

--- src/app/Components/Pages/products/product/product.component.ts
+++ src/app/Components/Pages/products/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { Product, ProductService } from "src/app/Shared/product.service";
 
 @Component({
   selector: "app-product",
@@ -7,18 +8,33 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./product.component.scss"],
 })
 export class ProductComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {}
   productId!: number;
+  product!: Product;
+  showcaseImageIds: number[] = [];
+  currentShowcaseId: number = 0;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
     this.route.params.subscribe((params) => {
       if (!params["id"]) return;
       if (isNaN(params["id"])) {
-        console.log('ji');
-        
         this.router.navigate(["../"], { relativeTo: this.route });
       }
-      this.productId = Number(params["id"]);
+      this.productId = this.currentShowcaseId = Number(params["id"]);
+      this.showcaseImageIds = new Array(6).fill(0).map(
+        (item, index) => (this.productId + index) % 16
+      );
     });
+    let product = await this.productService.getProductById(this.productId);
+    if (!product) {
+      this.router.navigate(["../"], { relativeTo: this.route });
+      return;
+    }
+    this.product = product;
   }
 }
--- src/app/Shared/product.service.ts
+++ src/app/Shared/product.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 export interface Product {
   id: number;
@@ -15,37 +15,32 @@ export interface Product {
 }
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ProductService {
   private products: Product[] = [];
-  productsURL: string = 'assets/JSON/products.json';
+  productsURL: string = "assets/JSON/products.json";
 
   constructor(private http: HttpClient) {}
 
   async getAllProducts(): Promise<Product[]> {
     this.products = await new Promise((resolve, reject) => {
-      this.http.get(this.productsURL).subscribe((data) => {
-        // resolve(data as Product[]);
-        setTimeout(() => {
-          resolve(data as Product[]);
-        }, 3000);
+      this.http.get<Product[]>(this.productsURL).subscribe((data) => {
+        resolve(data);
+        // setTimeout(() => {
+        //   resolve(data as Product[]);
+        // }, 3000);
       });
     });
     return this.products;
   }
-  async getProductsByCategory(
-    category: 'electronics' | 'beauty'
-  ): Promise<Product[]> {
-    this.products = await new Promise((resolve, reject) => {
-      this.http.get(this.productsURL).subscribe((data) => {
-        setTimeout(() => {
-          resolve(data as Product[]);
-        }, 3000);
+
+  async getProductById(productId: number): Promise<Product | undefined> {
+    return await new Promise((resolve, reject) => {
+      this.http.get<Product[]>(this.productsURL).subscribe((data) => {
+        const product = data.find((product) => product.id === productId);
+        resolve(product);
       });
     });
-    return this.products.filter(
-      (product: Product) => product.category === category
-    );
   }
 }
