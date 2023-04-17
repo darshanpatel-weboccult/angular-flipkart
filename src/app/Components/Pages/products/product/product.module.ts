9ee5d2c85fc5816c1c9a88b32bdab16fc9c4bc55
Darshan Patel <darshan.patel@weboccult.com>
2023-04-17 15:44:21 +0530

    Product: added ratings and reviews

--- src/app/Components/Pages/products/product/product.module.ts
+++ src/app/Components/Pages/products/product/product.module.ts
@@ -1,22 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProductComponent } from './product.component';
-import { BreadcrumbsModule } from 'src/app/Components/Partials/breadcrumbs/breadcrumbs.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductComponent } from "./product.component";
+import { BreadcrumbsModule } from "src/app/Components/Partials/breadcrumbs/breadcrumbs.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgCircleProgressModule } from "ng-circle-progress";
 
 @NgModule({
-  declarations: [
-    ProductComponent
-  ],
+  declarations: [ProductComponent],
   imports: [
     CommonModule,
     BreadcrumbsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgCircleProgressModule.forRoot({
+      outerStrokeWidth: 5,
+      showInnerStroke: true,
+      space: -5,
+      radius:28,
+      showBackground: false,
+      showUnits: false,
+      animation:false,
+      titleColor:'#212121',
+      titleFontSize:'16px',
+      titleFontWeight: '500',
+      innerStrokeWidth: 5,
+      showSubtitle: false,
+      innerStrokeColor: "#f0f0f0",
+      animationDuration:0
+    }),
   ],
-  exports:[
-    ProductComponent
-  ]
+  exports: [ProductComponent],
 })
-export class ProductModule { }
+export class ProductModule {}
